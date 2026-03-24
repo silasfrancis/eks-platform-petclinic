4dfd4cdc9b740eb2cb22b8d0effa30f1d764b8a4
silasfrancis <silasfrancis07@gmail.com>
2026-03-25 00:45:51 +0100

    update helm values for argo rollout

--- terraform/modules/ec2/sg-rules.tf
+++ terraform/modules/ec2/sg-rules.tf
@@ -10,12 +10,12 @@ resource "aws_vpc_security_group_egress_rule" "ec2_egress_rule" {
   }
 }
 resource "aws_vpc_security_group_egress_rule" "ec2_to_rds" {
-  security_group_id            = aws_security_group.ec2.id
-  referenced_security_group_id = aws_security_group.rds.id
-  from_port                    = 3306
-  to_port                      = 3306
-  ip_protocol                  = "tcp"
-  description                  = "Allow outbound MySQL to RDS"
+  description = "Allow outbound traffic from EC2 to RDS"
+  security_group_id = var.ec2_security_group_id
+  from_port = 3306
+  to_port = 3306
+  ip_protocol = "tcp"
+  cidr_ipv4   = "0.0.0.0/0"
   tags = { 
     resource = "vpc" 
   }

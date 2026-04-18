ce9a7264575b89ad96cd9ce1510636870f304798
silasfrancis <silasfrancis07@gmail.com>
2026-04-18 09:36:51 +0100

    final terraform clean up

--- terraform/modules/elb/policies.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_iam_policy" "lbc_policy" {
-  name   = "${var.env}-${var.app}-AWSLoadBalancerControllerIAMPolicy"
-  policy = file("${path.module}/lb-controller-policy.json")
-}
-
-resource "aws_iam_role_policy_attachment" "irsa_lb_controller_policy_attachment" {
-  role       = aws_iam_role.irsa_lb_controller.name
-  policy_arn = aws_iam_policy.lbc_policy.arn
-}

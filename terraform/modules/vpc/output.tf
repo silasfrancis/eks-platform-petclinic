1af50f62b5ceea1dc8a9b29a1a778b1313bb2515
silasfrancis <silasfrancis07@gmail.com>
2026-05-24 06:44:03 +0100

    vpc peering

--- terraform/modules/vpc/output.tf
+++ terraform/modules/vpc/output.tf
@@ -2,6 +2,10 @@ output "vpc_id" {
   value = aws_vpc.main_vpc.id
 }
 
+output "vpc_cidr_block" {
+  value = aws_vpc.main_vpc.cidr_block
+}
+
 output "public_subnet_ids" {
   value = [for subnet in aws_subnet.public : subnet.id]
 }
@@ -12,4 +16,18 @@ output "private_subnet_ids" {
 
 output "data_subnet_ids" {
   value = [for subnet in aws_subnet.data : subnet.id]
+}
+
+output "public_route_table_id" {
+  value       = aws_route_table.public.id
+}
+
+output "private_route_table_ids" {
+  description = "list of all private route table Ids"
+  value       = [for rt in aws_route_table.private : rt.id]
+}
+
+output "all_route_table_ids" {
+  description = "A unified list containing all public and private route table IDs" # a much needed output to pass to the vpc peering module
+  value       = concat([aws_route_table.public.id], [for rt in aws_route_table.private : rt.id])
 }

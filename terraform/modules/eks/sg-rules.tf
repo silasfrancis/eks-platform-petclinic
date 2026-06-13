c4d333581e503d6dbc909c6710003cf6377079e5
Silas Francis <123188381+silasfrancis@users.noreply.github.com>
2026-06-13 20:27:34 +0100

    main release pipeline fixes (#71)
    * Feature/silas (#17)
    
    * terraform fixes
    
    * terraform fixes
    
    * terraform fixes
    
    * terraform fixes
    
    * terraform cleanups
    
    * remove tfvars from tracking and update gitignore
    
    * terraform cleanups
    
    * terraform cleanups
    
    * terraform cleanups
    
    * terraform cleanups
    
    * fix: update kms policy and ensure state exclusion
    
    * ansible fixes
    
    * ansible fixes
    
    * ansible fixes
    
    * ansible fixes
    
    * ansible fixes
    
    * task files
    
    * task files
    
    * task files
    
    * task files
    
    * pipeline cleanup
    
    * fixing trivvy vul
    
    * fixing trivvy vul
    
    * fixing trivvy vul
    
    * fixing trivvy vul
    
    * fixing trivvy vul
    
    * fixing trivvy vul
    
    * fixing trivvy vul
    
    * fixing trivvy vul
    
    * fixing trivvy vul
    
    * fixing helm templates
    
    * fixing helm templates
    
    * fixing helm templates
    
    * fixing helm templates
    
    * fixing helm templates
    
    * fixing karpenter bootsrap node
    
    * fixing karpenter bootsrap node
    
    * fixing karpenter bootsrap node
    
    * karpenter bootstrap
    
    * karpenter bootstrap
    
    * karpenter bootstrap
    
    * karpenter bootstrap
    
    * finalizing helm env values (#18)
    
    * terraform fixes
    
    * terraform fixes
    
    * terraform fixes
    
    * terraform fixes
    
    * terraform cleanups
    
    * remove tfvars from tracking and update gitignore
    
    * terraform cleanups
    
    * terraform cleanups
    
    * terraform cleanups
    
    * terraform cleanups
    
    * fix: update kms policy and ensure state exclusion
    
    * ansible fixes
    
    * ansible fixes
    
    * ansible fixes
    
    * ansible fixes
    
    * ansible fixes
    
    * task files
    
    * task files
    
    * task files
    
    * task files
    
    * pipeline cleanup
    
    * fixing trivvy vul
    
    * fixing trivvy vul
    
    * fixing trivvy vul
    
    * fixing trivvy vul
    
    * fixing trivvy vul
    
    * fixing trivvy vul
    
    * fixing trivvy vul
    
    * fixing trivvy vul
    
    * fixing trivvy vul
    
    * fixing helm templates
    
    * fixing helm templates
    
    * fixing helm templates
    
    * fixing helm templates
    
    * fixing helm templates
    
    * fixing karpenter bootsrap node
    
    * fixing karpenter bootsrap node
    
    * fixing karpenter bootsrap node
    
    * karpenter bootstrap
    
    * karpenter bootstrap
    
    * karpenter bootstrap
    
    * karpenter bootstrap
    
    * karpenter bootstrap
    
    * karpenter bootstrap
    
    * finalizing env values
    
    * finalizing env values
    
    * finalizing env values
    
    * finalizing env values
    
    * sync with main
    
    * sync with main
    
    * sync with main
    
    * sync with main
    
    * sync with main
    
    * sync with main
    
    * sync with main
    
    * sync with main
    
    * sync with main

---
+++ terraform/modules/eks/sg-rules.tf
@@ -0,0 +1,57 @@
+resource "aws_vpc_security_group_ingress_rule" "eks_from_wireguard_server" {
+    description = "Allow EKS control plane access from wireguard server"
+    security_group_id = aws_eks_cluster.main_cluster.vpc_config[0].cluster_security_group_id
+    from_port = 443
+    to_port = 443
+    ip_protocol = "tcp"
+    referenced_security_group_id = var.wireguard_server_security_group_id
+    tags = {
+    resource = "vpc"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "control_plane_to_nodes" {
+  description       = "Allow Cluster Control Plane to communicate with pods"
+  security_group_id = var.eks_node_sg_id
+  ip_protocol       = "-1" 
+  referenced_security_group_id = aws_eks_cluster.main_cluster.vpc_config[0].cluster_security_group_id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "nodes_internal" {
+  description       = "Allow nodes to communicate with each other"
+  security_group_id = var.eks_node_sg_id
+  ip_protocol       = "-1" 
+  referenced_security_group_id = var.eks_node_sg_id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "external_nlb_to_node" {
+  description       = "Allow External NLB to reach node port for HTTP"
+  security_group_id = var.eks_node_sg_id
+  
+  from_port         = 30000
+  to_port           = 32767
+  ip_protocol       = "tcp"
+  referenced_security_group_id = var.nlb_external_security_group_id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "nlb_to_nodes_healthcheck" {
+  description       = "Allow NLB to reach node port for Istio health check"
+  security_group_id = var.eks_node_sg_id 
+  
+  from_port         = 32319
+  to_port           = 32319
+  ip_protocol       = "tcp"
+  referenced_security_group_id = var.nlb_external_security_group_id
+}
+
+resource "aws_vpc_security_group_egress_rule" "nodes_to_internet" {
+  description       = "Allow nodes to reach AWS APIs and Internet for bootstrapping"
+  security_group_id = var.eks_node_sg_id
+  
+  ip_protocol       = "-1"
+  cidr_ipv4         = "0.0.0.0/0"
+  
+  tags = {
+    Name = "node-egress-all"
+  }
+}
